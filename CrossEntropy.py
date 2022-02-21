0bfdf2efc1394cc8c3aa258c877b537d937fc475
kass2022 <kass_zero@163.com>
2022-02-21 18:09:08 +0800

    update crossentropy file

--- CrossEntropy.py
+++ CrossEntropy.py
@@ -1,3 +1,4 @@
+from tkinter import X
 import numpy as np
 from GravityModel import underground
 
@@ -21,7 +22,7 @@ class data():
         self.directional = directional(self.property.shape)
         self.gradient = np.zeros(self.property.shape)
 
-    def compute_directional(self):
+    def computeDirectional(self):
 
         for i in range(1, self.x_num - 1):
             for j in range(1, self.y_num - 1):
@@ -50,104 +51,159 @@ class CrossEntropy():
 
         self.x_num, self.y_num, self.z_num = self.property1.property.shape
 
-    def compute_CrossEntropy(self):
+    def computeCrossEntropy(self):
 
-        self.property1.compute_directional()
-        self.property2.compute_directional()
+        self.property1.computeDirectional()
+        self.property2.computeDirectional()
 
-        self.cross_entropy.x = self.property1.directional.y * self.property2.directional.z - self.property1.directional.z * self.property2.directional.y
-        self.cross_entropy.y = self.property1.directional.x * self.property2.directional.z - self.property1.directional.z * self.property2.directional.x
-        self.cross_entropy.z = self.property1.directional.x * self.property2.directional.y - self.property1.directional.y * self.property2.directional.x        
+        self.cross_entropy.x = self.property1.directional.y * self.property2.directional.z \
+            - self.property1.directional.z * self.property2.directional.y
+        self.cross_entropy.y = self.property1.directional.x * self.property2.directional.z \
+            - self.property1.directional.z * self.property2.directional.x
+        self.cross_entropy.z = self.property1.directional.x * self.property2.directional.y \
+            - self.property1.directional.y * self.property2.directional.x        
         self.cross_entropy.whole = self.cross_entropy.x**2 + self.cross_entropy.y**2 + self.cross_entropy.z**2
 
-    # def compute_gradient_of_CrossEntropy(self):
+    def computeGradient(self):
 
-    #     def core_fun(data1, data2, i, j, k):
+        def computeGradientIJK(data1,data2,i,j,k):
 
-    #         temp_gradient = (data2.directional.y[i, j, k] * self.cross_entropy.x[i, j, k] \
-    #             + data2.directional.z[i, j, k] * self.cross_entropy.y[i, j, k]) / self.x_step
-    #         data1.gradient[i - 1, j, k] += temp_gradient
-    #         data1.gradient[i + 1, j, k] -= temp_gradient
+            temp_gradient = self.cross_entropy.y[i,j,k]*data2.directional.z[i,j,k]/self.x_step \
+                + self.cross_entropy.z[i,j,k]*data2.directional.y[i,j,k]/self.x_step
+            if temp_gradient!= 0:
+                data1.gradient[i - 1, j, k] -= temp_gradient
+                data1.gradient[i + 1, j, k] += temp_gradient
 
-    #         temp_gradient = (data2.directional.x[i, j, k] * self.cross_entropy.z[i, j, k] \
-    #             + data2.directional.z[i, j, k] * self.cross_entropy.x[i, j, k]) / self.y_step
-    #         data1.gradient[i, j - 1, k] += temp_gradient
-    #         data1.gradient[i, j + 1, k] -= temp_gradient
+            temp_gradient = self.cross_entropy.x[i,j,k]*data2.directional.z[i,j,k]/self.y_step\
+                +self.cross_entropy.z[i,j,k]*data2.directional.x[i,j,k]/self.y_step
+            if temp_gradient != 0:
+                data1.gradient[i,j-1,k] -= temp_gradient
+                data1.gradient[i,j+1,k] += temp_gradient   
 
-    #         temp_gradient = (data2.directional.x[i, j, k] * self.cross_entropy.y[i, j, k] \
-    #             + data2.directional.y[i, j, k] * self.cross_entropy.x[i, j, k]) / self.z_step
-    #         data1.gradient[i, j, k - 1] += temp_gradient
-    #         data1.gradient[i, j, k + 1] -= temp_gradient
+            temp_gradient = self.cross_entropy.x[i,j,k]*data2.directional.y[i,j,k]/self.z_step\
+                -self.cross_entropy.y[i,j,k]*data2.directional.x[i,j,k]/self.z_step   
+            if temp_gradient!= 0:
+                data1.gradient[i,j,k-1] -= temp_gradient
+                data1.gradient[i,j,k+1] += temp_gradient   
 
-    #     for i in range(1, self.x_num - 1):
-    #         for j in range(1, self.y_num - 1):
-    #             for k in range(1, self.z_num - 1):
-    #                 core_fun(self.property1, self.property2, i, j, k)
-    #                 core_fun(self.property2, self.property1, i, j, k)
-
-
-    def compute_gradient_of_CrossEntropy(self):
-
-        self.compute_CrossEntropy()
-
-        def gradient_of_tx(data1, data2, i, j, k):
-            
-            if self.cross_entropy.x[i, j, k] != 0:
-
-                gradient = np.zeros(self.property1.property.shape)
-
-                temp_gradient = data2.directional.z[i, j, k] / 2 * self.y_step
-                gradient[i, j - 1, k] = + temp_gradient
-                gradient[i, j + 1, k] = - temp_gradient
-
-                temp_gradient = data2.directional.y[i, j, k] / 2 * self.z_step
-                gradient[i, j, k - 1] = - temp_gradient
-                gradient[i, j, k + 1] = + temp_gradient
-
-                data1.gradient += gradient * self.cross_entropy.x[i, j, k]
-
-        def gradient_of_ty(data1, data2, i, j, k):
-            
-            if self.cross_entropy.y[i, j, k] != 0:
-
-                gradient = np.zeros(self.property1.property.shape)
-
-                temp_gradient = data2.directional.z[i, j, k] / 2 * self.x_step
-                gradient[i - 1, j, k] = + temp_gradient
-                gradient[i + 1, j, k] = - temp_gradient
-
-                temp_gradient = data2.directional.x[i, j, k] / 2 * self.z_step
-                gradient[i, j, k - 1] = - temp_gradient
-                gradient[i, j, k + 1] = + temp_gradient
-
-                data1.gradient += gradient * self.cross_entropy.y[i, j, k]
-
-        def gradient_of_tz(data1, data2, i, j, k):
-
-            if self.cross_entropy.z[i, j, k] != 0:
-                
-                gradient = np.zeros(self.property1.property.shape)
-
-                temp_gradient = data2.directional.y[i, j, k] / 2 * self.x_step
-                gradient[i - 1, j, k] = + temp_gradient
-                gradient[i + 1, j, k] = - temp_gradient
-
-                temp_gradient = data2.directional.x[i, j, k] / 2 * self.y_step
-                gradient[i, j - 1, k] = - temp_gradient
-                gradient[i, j + 1, k] = + temp_gradient
-
-                data1.gradient += gradient * self.cross_entropy.z[i, j, k]
+        self.computeCrossEntropy()
 
         for i in range(1, self.x_num - 1):
             for j in range(1, self.y_num - 1):
                 for k in range(1, self.z_num - 1):
+                    computeGradientIJK(self.property1, self.property2, i, j, k)
 
-                    gradient_of_tx(self.property1, self.property2, i, j, k)
-                    gradient_of_tx(self.property2, self.property1, i, j, k)
-                    gradient_of_ty(self.property1, self.property2, i, j, k)
-                    gradient_of_ty(self.property2, self.property1, i, j, k)
-                    gradient_of_tz(self.property1, self.property2, i, j, k)
-                    gradient_of_tz(self.property2, self.property1, i, j, k)
-                
-                
+if __name__ == "__main__":
+
+    # define 2 models of different properties : m1 and m2
+    x_num = 100
+    y_num = 100
+    z_num = 100
+
+    m1 = np.zeros((x_num,y_num,z_num))
+    m1[40:60, 30:70, 20:80] = 1
+
+    m2 = np.zeros((x_num,y_num,z_num))
+    for i in range(x_num):
+        m2[:,:,i] = i
+
+    # compute crossentropy of m1 and m2, also its gradient
+    x_step = 1000
+    y_step = 1000
+    z_step = 1000
+    t = CrossEntropy(m1, m2, x_step, y_step, z_step)
+    t.computeGradient()    
+
+    # show the results
+    
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    figsize1 = 3
+    figsize2 = 3
+    slice_y = 50
+    i = 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(m1[:,slice_y,:].T)
+    plt.colorbar()
+    plt.title("m1")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(m2[:,slice_y,:].T)
+    plt.colorbar()
+    plt.title("m2")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.cross_entropy.x[:,slice_y,:].T)
+    plt.colorbar()
+    plt.title("tx")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.cross_entropy.y[:,slice_y,:].T)
+    plt.colorbar()
+    plt.title("ty")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.cross_entropy.z[:,slice_y,:].T)
+    plt.colorbar()
+    plt.title("tz")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.cross_entropy.whole[:,slice_y,:].T)
+    plt.colorbar()
+    plt.title("t")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.property1.gradient[:,slice_y,:].T)
+    plt.colorbar()
+    plt.title("t_gradient")
+
+    plt.suptitle("y = 50")
+
+    plt.figure()
+    figsize1 = 3
+    figsize2 = 3
+    slice_z = 50
+    i = 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(m1[:,:,slice_z].T)
+    plt.colorbar()
+    plt.title("m1")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(m2[:,:,slice_z].T)
+    plt.colorbar()
+    plt.title("m2")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.cross_entropy.x[:,:,slice_z].T)
+    plt.colorbar()
+    plt.title("tx")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.cross_entropy.y[:,:,slice_z].T)
+    plt.colorbar()
+    plt.title("ty")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.cross_entropy.z[:,:,slice_z].T)
+    plt.colorbar()
+    plt.title("tz")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.cross_entropy.whole[:,:,slice_z].T)
+    plt.colorbar()
+    plt.title("t")
+    i += 1
+    plt.subplot(figsize1,figsize2,i)
+    plt.imshow(t.property1.gradient[:,:,slice_z].T)
+    plt.colorbar()
+    plt.title("t_gradient")
+
+    plt.suptitle("z = 50")
+
+    plt.show()
+
+    
+          
 
